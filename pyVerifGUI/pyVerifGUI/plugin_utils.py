68a71d3f96d47fd87bad13410292824341e826ee
David Lenfesty <david.lenfesty@eideticom.com>
2020-12-07 16:30:27 -0700

    pyVerifGUI: directly compile plugins
    This removes some inconsistencies that would be annoying to debug as a
    user doing plugin development and trying to install some.

--- pyVerifGUI/pyVerifGUI/plugin_utils.py
+++ pyVerifGUI/pyVerifGUI/plugin_utils.py
@@ -10,34 +10,38 @@
 #
 # @brief Utilities common for the task and tab plugin systems.
 ##############################################################################
-from importlib import import_module, invalidate_caches
 from typing import Callable, Any, List
+from types import ModuleType
 from pathlib import Path
 import inspect
-import sys
 import os
 
 def import_plugins(directories: List[os.PathLike], verify: Callable[[Any], bool]) -> List:
     """Imports objects from all python files in the given directories. Then
-    uses verify to filter to the valid plugin objects"""
+    uses verify to filter to the valid plugin objects.
+
+    Takes the first plugin of a given name, if your plugin is not appearing
+    check for naming conflicts."""
     plugins = []
+    plugin_names = []
 
     directories = [Path(path) for path in directories]
     for dir in directories:
         if dir.is_dir():
-            # Add to system search path
-            sys.path.insert(0, str(dir))
-
             for module in dir.iterdir():
                 if module.name == "__init__.py" or module.suffix != ".py":
                     continue
 
-                invalidate_caches()
-                mod = import_module(str(module.stem))
+                # I *would* use importlib, but that opens issues with
+                # prior imports and can mess with importing things properly.
+                mod = ModuleType(module.name)
+                mod.__file__ = str(module.resolve())
+                incoming_file = open(str(module)).read()
+                exec(incoming_file, mod.__dict__)
                 for cls in inspect.getmembers(mod, inspect.isclass):
                     if verify(cls[1]):
-                        plugins.append(cls[1])
-
-            sys.path.pop(0)
+                        if cls[0] not in plugin_names:
+                            plugins.append(cls[1])
+                            plugin_names.append(cls[0])
 
     return plugins
